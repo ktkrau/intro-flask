b661192f402d92153768c4f25b2a609f0f2e8da6
ktkrau <ktkrau@gmail.com>
2022-09-20 19:54:01 -0300

    fin

---
+++ hello.py
@@ -0,0 +1,16 @@
+from flask import Flask #Importando Flask para permitirnos crear una aplicacion
+
+app = Flask(__name__) #Creando una nueva instancia de la clase Flask llamada "app"
+
+@app.route('/') #El decorador '@' asocia esa ruta con la funcion que vamos a tener inmediatamente
+def hola_mundo():
+    return 'Hola Mundo!'
+
+@app.route('/hola/<nombre>') #para mi ruta hola/___ se va a pasar como variable nombre
+def hola(nombre):
+    return f'Hola {nombre}, cómo estás?'
+
+
+if __name__ == "__main__": #asegura que el archivo se esta ejecutando directamente y no desde un modulo diferente
+    app.run(debug=True) #ejecuta la aplicacion 
+
