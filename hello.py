8b5766b5fe0b777d298f38526749b8c8cdaed924
ktkrau <ktkrau@gmail.com>
2022-09-21 11:30:56 -0300

    templates y static

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
-from flask import Flask #Importando Flask para permitirnos crear una aplicacion
 # pipenv install flask 
 # pipenv shell 
 # python3 nombre archivo.py
+
+from flask import Flask, render_template #Importando Flask para permitirnos crear una aplicacion
+
 
 app = Flask(__name__) #Creando una nueva instancia de la clase Flask llamada "app"
 
@@ -12,6 +14,11 @@ def hola_mundo():
 @app.route('/hola/<nombre>') #para mi ruta hola/___ se va a pasar como variable nombre
 def hola(nombre):
     return f'Hola {nombre}, cómo estás?'
+
+@app.route('/hello/<name>/<int:j>')
+def hello(name, j):
+    # en lugar de devolver una cadena devolvemos el resultado de un render template
+    return render_template('hola.html', nombre=name, cantidad =j)
 
 
 if __name__ == "__main__": #asegura que el archivo se esta ejecutando directamente y no desde un modulo diferente
